9f02940e86527e5bfa81f6eccefb66f2904a2718
hakamfaza <hakamfaza@gmail.com>
2022-03-07 13:28:11 +0700

    first commit

---
+++ tugas.js
@@ -0,0 +1,9 @@
+let angka = 15;
+
+for (let i = 0; i <= angka; i++) {
+    if (i % 2 === 0) {
+        console.log(`${i} genap`);
+    } else {
+        console.log(`${i} ganjil`)
+    }
+}
---
+++ week2.js
@@ -0,0 +1,7 @@
+const biodata = {
+    name: 'Muhamad Hakam Faza',
+    age : 19,
+    hobbies: ['Membaca buku', 'Mendengarkan musik', 'Bermain laptop'],
+    isMarried: false,
+    schoolList: ''
+}
